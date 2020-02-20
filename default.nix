88679088be7e5bc44c46c80943300ef89f59136b
Damien Nicolas <damien@gordon.re>
2020-02-20 11:16:33 +0100

    Add mbsync_tools derivation

---
+++ default.nix
@@ -0,0 +1,5 @@
+with <nixpkgs> {};
+
+{
+  mbsync_tools = import ./mbsync_tools.nix;
+}
---
+++ mbsync_tools.nix
@@ -0,0 +1,13 @@
+with import <nixpkgs> {};
+with pkgs.python3Packages;
+
+buildPythonPackage rec {
+  name = "mbsync_tools";
+  src = fetchFromGitHub {
+    owner = "gordonzola";
+    repo = "mbsync_tools";
+    rev = "v0.1.0";
+    sha256 = "01jxz7bdlzaj0sxnr9pc92mv2p645a7d9cq0jgphmqgsi4fqbp00";
+  };
+  propagatedBuildInputs = [ pkgs.libsecret pkgs.python37Packages.secretstorage pkgs.python37Packages.keyring ];
+}
