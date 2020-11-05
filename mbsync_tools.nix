571cf081febd3181c360484da65176e085d52218
Damien Nicolas <damien@gordon.re>
2020-11-05 11:12:32 +0100

    Add python3.8 support for mbsync_tools

--- mbsync_tools.nix
+++ mbsync_tools.nix
@@ -9,5 +9,5 @@ buildPythonPackage rec {
     rev = "v0.1.0";
     sha256 = "01jxz7bdlzaj0sxnr9pc92mv2p645a7d9cq0jgphmqgsi4fqbp00";
   };
-  propagatedBuildInputs = [ pkgs.libsecret pkgs.python37Packages.secretstorage pkgs.python37Packages.keyring ];
+  propagatedBuildInputs = [ pkgs.libsecret pkgs.python37Packages.secretstorage pkgs.python37Packages.keyring pkgs.python38Packages.secretstorage pkgs.python38Packages.keyring ];
 }
