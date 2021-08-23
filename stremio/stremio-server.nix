9ead5192234e39f20d07a5ba80eb1b7d25abd725
Gaëlle Robardet <git@gaelle.pink>
2021-08-23 18:03:38 +0200

    Fix stremio server hash

--- stremio/stremio-server.nix
+++ stremio/stremio-server.nix
@@ -8,13 +8,13 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "https://dl.strem.io/four/v${version}/server.js";
     # sha256 = "01dc8l1xvwjjg94ycc0i1kwrfjrigs55g56d6a7l63ysl4xk26pa";
-    sha256 = "1idxfdzp4zxqjihq4m520kp02kaqb8hw82w4y5vxajgc040mhq0m";
+    sha256 = "127rfyraa59cx5r7nlvpnsclnkrylvahl3q7g4qz80sz670jywks";
   };
 
   asarFile = fetchurl {
     url = "https://dl.strem.io/four/v${version}/stremio.asar";
     # sha256 = "1jkr377dlr5m5zm7n3lsvz3lmnqhipzb9r1szkrinfab865ck50g";
-    sha256 = "1jkr377dlr5m5zm7n3lsvz3lmnqhipzb9r1szkrinfab865ck50g";
+    sha256 = "1d8xy9xj4x6pip3cwkqrn2ni4szs50bxynl1ijz5kqzipy16ky63";
   };
 
   buildInputs = [
