e95fbfc5c2c30bb5a3af9fd09ea17f8f0fe5b537
Gaëlle Robardet <git@gaelle.pink>
2021-12-07 17:10:17 +0100

    Bump mbsync_helpers

--- mbsync_tools.nix
+++ mbsync_tools.nix
@@ -4,10 +4,11 @@ with pkgs.python3Packages;
 buildPythonPackage rec {
   name = "mbsync_tools";
   src = fetchFromGitHub {
-    owner = "gordonzola";
+    owner = "vegaelle";
     repo = "mbsync_tools";
     rev = "v0.1.0";
     sha256 = "01jxz7bdlzaj0sxnr9pc92mv2p645a7d9cq0jgphmqgsi4fqbp00";
   };
-  propagatedBuildInputs = [ pkgs.libsecret pkgs.python37Packages.secretstorage pkgs.python37Packages.keyring pkgs.python38Packages.secretstorage pkgs.python38Packages.keyring ];
+  propagatedBuildInputs = [ pkgs.libsecret pkgs.python3Packages.secretstorage
+  pkgs.python3Packages.keyring ];
 }
